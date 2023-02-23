edc842d04055aaa9937f712569555327c6d014c6
Vitalii <vagontravki16@gmail.com>
2023-02-23 10:36:41 +0300

    Сохранил последние модификации

--- Exemple001_Hello_console/Program.cs
+++ Exemple001_Hello_console/Program.cs
@@ -1,1 +1,18 @@
 ﻿Console.WriteLine(" 2 Hello, World!");
+
+Console.Write("Введите пятизначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int tmp = N;
+int num1 = N / 1000;
+string num2 = String.Empty;
+if (N / 10000 >= 1 && N / 10000 <= 9)
+{
+    for (int i = 0; i < 2; i++)
+    {
+        num2 = num2 + $"{N % 10}";
+        N = N / 10;
+    }
+    if (num1 == Convert.ToInt32(num2)) Console.WriteLine($"{tmp} -> Да");
+    else Console.WriteLine($"{tmp} -> Нет");
+}
+else Console.WriteLine($"{tmp} - Число не пятизначное!");
